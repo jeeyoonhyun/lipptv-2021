bdad0e4f7de694ffab3bbab6a226ed7a275769d4
Jeeyoon Hyun <metasekuoia@gmail.com>
2021-04-29 15:02:36 -0400

    styling for program section

--- src/Program.js
+++ src/Program.js
@@ -1,20 +1,32 @@
 function Program() {
     return (
-        <div className='section-program' id='program'>
-            <div className='title' id='title-program'>PROGRAM</div>
+        <div className='section section-program' id='program'>
+            <div className='title title-program'>PROGRAM</div>
             {/* time */}
-            <div className='headline' id='headline-program'>7:30 PM – 8:00 PM</div>
-            {/* accordion menu */}
-            <div className='bodytext' id='bodytext-program'>INTRODUCTION WITH LIPP TV HOST PROFESSOR LIPP + ADVERTISMENT BATCH #1</div>
-            <div className='headline' id='program-performers'>▷ MONDOFACTS HOSTED BY SYLVAN ZHENG</div>
-            <div className='headline' id='program-performers'>▷ MTV JAMS HOSTED BY NIKHIL KUMAR</div>
-            <div className='headline' id='program-performers'>▷ NO RESEARCH NATURE HOSTED BY BEN MOLL</div>
+            <div className='section-time'>
+                <div className='headline headline-time' >7:30 PM – 8:00 PM</div>
+                <div className='bodytext bodytext-program'>INTRODUCTION WITH LIPP TV HOST PROFESSOR LIPP + ADVERTISMENT BATCH #1</div>
+            </div>
+            {/* accordion menus */}
+            <div className='section-performers'>
+                <div className='headline program-performers program-red'>▷ MONDOFACTS HOSTED BY SYLVAN ZHENG</div>
+                <div className='headline program-performers program-magenta'>▷ MTV JAMS HOSTED BY NIKHIL KUMAR</div>
+                <div className='headline program-performers program-blue'>▷ NO RESEARCH NATURE HOSTED BY BEN MOLL</div>
+            </div>
 
-            <div className='headline' id='headline-program'>8:00 PM – 8:30 PM</div>
-            <div className='bodytext' id='bodytext-program'>INTRODUCTION WITH LIPP TV HOST PROFESSOR LIPP + ADVERTISMENT BATCH #2</div>
-            <div className='headline' id='program-performers'>▷ STREAM FIGHTER HOSTED BY CHRISTINA DACANAY AND CY KIM</div>
-            <div className='headline' id='program-performers'>▷ OFFICE OLYMPICS HOSTED BY CEZAR MOCAN</div>
-            <div className='headline' id='program-performers'>▷ RANDOM ACCESS MEMORIES HOSTED BY SOHAILA MOSBEH AND YOUNG MIN CHOI</div>
+            {/* time */}
+            <div className='section-time'>
+                <div className='headline headline-time'>I8:00 PM – 8:30 PM</div>
+                <div className='bodytext bodytext-program'>INTRODUCTION WITH LIPP TV HOST PROFESSOR LIPP 
+                + ADVERTISMENT BATCH #2</div>
+            </div>
+            {/* accordion menus */}
+            <div className='section-performers'>
+                <div className='headline program-performers program-yellow'>▷ STREAM FIGHTER HOSTED BY CHRISTINA DACANAY AND CY KIM</div>
+                <div className='headline program-performers program-magenta'>▷ OFFICE OLYMPICS HOSTED BY CEZAR MOCAN</div>
+                <div className='headline program-performers program-white'>▷ RANDOM ACCESS MEMORIES HOSTED BY SOHAILA MOSBEH AND YOUNG MIN CHOI</div>
+            </div>
+
 
         </div>
 
