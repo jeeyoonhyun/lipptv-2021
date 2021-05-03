d5a4dd34d5c4b878f1ef7e238cf0e04d2704f056
Katie Han <katie@normalvr.com>
2021-05-03 09:13:29 -0400

    small fixes

--- src/About.js
+++ src/About.js
@@ -8,10 +8,10 @@ function About() {
             </div>
             <div className='section-inner-half'>
                 <div className='title text-small bodytext-about about-heading'>ABOUT</div>
-                <div className='text-small bodytext-about'>LIPP TV IS A CREATIVE RESPONSE TO HOW CODE, VIDEO NETWORKS, AND ART CAN BE USED TO CREATE A NEW EXPERIENCE IN LIVE PERFORMANCE. EACH STUDENT HAS CREATED THEIR OWN SHORT TV SHOW INFLUENCED BY VIDEO ART, EXPERIMENTAL ANIMATION, PUBLIC ACCESS TV, AND MORE.
-                THIS EVENT WAS CREATED REMOTELY AND IS PERFORMED REMOTELY, WITH STUDENTS CREATING THE WEBSITE, COMMERCIALS, ANIMATIONS, AND MORE.
-                TUNE IN TO LIPP TV AND MAKE SURE TO JOIN THE CHAT TO TALK WITH OUR CREATORS AND PARTICIPATE IN THEIR SHOWS.
-                </div>    
+                <div className='text-small bodytext-about'>LIPP TV IS A CREATIVE RESPONSE TO HOW CODE, VIDEO NETWORKS, AND ART CAN BE USED TO CREATE A NEW EXPERIENCE IN LIVE PERFORMANCE.</div>
+                <div className='text-small bodytext-about'><br></br>EACH STUDENT HAS CREATED THEIR OWN SHORT TV SHOW INFLUENCED BY VIDEO ART, EXPERIMENTAL ANIMATION, PUBLIC ACCESS TV, AND MORE.</div>
+                <div className='text-small bodytext-about'><br></br>THIS EVENT WAS CREATED REMOTELY AND IS PERFORMED REMOTELY, WITH STUDENTS CREATING THE WEBSITE, COMMERCIALS, ANIMATIONS, AND MORE.</div>
+                <div className='text-small bodytext-about'><br></br>TUNE IN TO LIPP TV AND MAKE SURE TO JOIN THE CHAT TO TALK WITH OUR CREATORS AND PARTICIPATE IN THEIR SHOWS.</div>    
             </div>
         </div>
     );
